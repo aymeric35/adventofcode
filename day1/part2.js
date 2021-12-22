adf059ea6ddb19788cc2f1e96493709567c057e9
Aymeric <aymeric.bertrand35@gmail.com>
2021-12-22 10:54:25 +0100

    Add increaseCount function

--- day1/part2.js
+++ day1/part2.js
@@ -3,3 +3,17 @@ const path = require('path');
 const source = fs.readFileSync(path.resolve(__dirname, './input.txt')).toString();
 const arrayFromSource = source.split('\n').map(number => parseInt(number, 10));
 
+function increaseCount(array) {
+    let total = 0;
+    for (let i = 3; i < array.length; i++) {
+        const current = array[i - 1] + array[i - 2] + array[i - 3];
+        const next = array[i] + array[i - 1] + array[i - 2];
+
+        if (next > current) {
+            total += 1;
+        }
+    }
+    return total
+}
+
+increaseCount(arrayFromSource);
