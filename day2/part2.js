3a67d59fda810666c668547ea98ec0d9ca885cf8
Aymeric <aymeric.bertrand35@gmail.com>
2021-12-22 11:45:30 +0100

    Create part2.js

---
+++ day2/part2.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = require('path');
+const source = fs.readFileSync(path.resolve(__dirname, './input.txt')).toString();
+const arrayFromSource = source.split('\n');
