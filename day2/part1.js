4f8c5b8baa1f423b15575266960583e99127ee1b
Aymeric <aymeric.bertrand35@gmail.com>
2021-12-22 14:07:03 +0100

    Fix typo

--- day2/part1.js
+++ day2/part1.js
@@ -4,38 +4,39 @@ const source = fs.readFileSync(path.resolve(__dirname, './input.txt')).toString(
 const arrayFromSource = source.split('\n');
 
 function getPosition(array) {
-    let value;
+    let store;
     let up = 0;
     let down = 0;
     let forward = 0;
     for (let i = 0; i < array.length; i++) {
         if (array[i].includes('forward')) {
-            value = array[i].replace('forward ', '');
-            value = parseInt(value, 10)
-            forward += value
+            store = array[i].replace('forward ', '');
+            store = parseInt(store, 10);
+            forward += store;
         }
-        if (array[i].includes('up')) {
-            value = array[i].replace('up ', '');
-            value = parseInt(value, 10)
-            up += value
+        else if (array[i].includes('up')) {
+            store = array[i].replace('up ', '');
+            store = parseInt(store, 10);
+            up += store;
         }
-        if (array[i].includes('down')) {
-            value = array[i].replace('down ', '');
-            value = parseInt(value, 10)
-            down += value
+        else if (array[i].includes('down')) {
+            store = array[i].replace('down ', '');
+            store = parseInt(store, 10);
+            down += store;
         }
     }
-    return (down - up) * forward
+    return (down - up) * forward;
 }
 
 getPosition(arrayFromSource);
-
 
 // J'ai d'abord résolu le problème avec le code ci-dessous mais je me suis rendu compte
 // que celui-ci n'était pas vraiment optimisé à cause du nombre de boucles
 // qui sont exécutées. J'ai donc à nouveau opté pour une boucle for qui me semblait
 // plus adaptée dans ce cas précis. J'ai tout de même souhaité laisser le code que j'avais
-// précédemment écrit ci-dessous.
+// précédemment écrit ci-dessous. Un autre problème que j'ai constaté avec ce code était
+// qu'il ne considérait pas l'ordre des instructions du fichier input. Je me contentais
+// d'additionner la somme des différents éléments, ce qui devient problématique dans la partie 2.
 
 function alternativeMethod() {
     function filterDirection(array) {
